3bea0b9a435ab3459e6a3871b0eba5128e744405
corinnegottleb <corinneottlieb@gmail.com>
2021-05-20 11:30:42 +0300

    counter to undo like

--- src/Tweet.js
+++ src/Tweet.js
@@ -28,14 +28,42 @@ const Reply = () => <i className="fa fa-reply reply-button" />;
 
 const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
 
-const LikeButton = ({id, likeTweet}) => {
-  const [color, setColor] = useState(false);
-  const handleClick = async ()=>{
-    setColor(!color)
-    await likeTweet(id)
+const LikeButton = ({ id, likeTweet }) => {
+  const [counter, setCounter] = useState(0)
+  const [liked, setLiked] = useState(false)
+
+  const handleClick = () => {
+    setLiked(!liked)
+    if (counter === 0) {
+      setCounter(3)
+    }
   }
 
-  return <i onClick={handleClick} className={color? `fa fa-heart like-button liked`: "fa fa-heart like-button"} />;
+  React.useEffect(() => {
+    const like = async (id) => {
+      await likeTweet(id).catch((err) => {
+        console.log(err)
+        setLiked(false)
+      })
+    }
+
+    if (counter > 0) {
+      setTimeout(() => setCounter(counter - 1), 1000);
+    } else {
+      setCounter(0);
+      if (liked) {
+        like(id)
+      }
+    }
+  }, [counter, likeTweet, liked, id]);
+
+
+  return (
+    <>
+      {counter ? <span>{counter}</span> : null}
+      <i onClick={handleClick} className={liked ? `fa fa-heart like-button liked` : "fa fa-heart like-button"} />
+    </>
+  )
 }
 
 const MoreOPtionsButton = () => (
