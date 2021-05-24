d50a4ba850797f452b57600428cd52ccdf46642f
Nadav_Frank <nadavf@misterbit.co.il>
2021-05-24 15:11:29 +0300

    final code edit

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, } from "react";
+import React, { useState } from "react";
 import Avatar from "./Avatar";
 import Message from "./Message";
 import NameWithHandle from "./NameWithHandle";
@@ -28,7 +28,7 @@ export default function Tweet({ tweet }) {
   return (
     <div className="tweet">
       <div >
-        {isOpen && <Popup setIsOpen={setIsOpen} onAbort={onAbort} onRequestCompleted={onRequestCompleted} togglePopup={() => setIsAborted(true)} />}
+        {isOpen && <Popup setIsOpen={setIsOpen} onAbort={onAbort} onRequestCompleted={onRequestCompleted} />}
       </div>
       <Avatar />
       <div className="content">
